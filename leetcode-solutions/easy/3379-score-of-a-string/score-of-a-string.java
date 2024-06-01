161839e3fdfda4225e84fe7e5f1b874348a38e3a
Kapil <66022116+immortals-ume@users.noreply.github.com>
2024-06-01 16:30:13 +0530

    Time: 5 ms (99.87%) | Memory: 42.5 MB (7.67%) - LeetSync

--- leetcode-solutions/easy/3379-score-of-a-string/score-of-a-string.java
+++ leetcode-solutions/easy/3379-score-of-a-string/score-of-a-string.java
@@ -1,9 +1,7 @@
 class Solution {
     public int scoreOfString(String s) {
-        int sum = 0;
-        for(int i =0;i<s.length()-1;i++){ 
-            sum+=Math.abs(s.charAt(i)-s.charAt(i+1));
-        }
-        return sum;
+      return IntStream.range(0, s.length() - 1)
+                        .map(i -> Math.abs(s.charAt(i) - s.charAt(i + 1)))
+                        .sum();
     }
 }
