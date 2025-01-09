d2d6e92e47c1d3421f901a608eb820b3a01b4a9a
Kapil <66022116+immortals-ume@users.noreply.github.com>
2025-01-10 00:49:21 +0530

    Time: 15 ms (54.02%) | Memory: 52.6 MB (31.38%) - LeetSync

--- leetcode-solutions/easy/816-design-hashset/design-hashset.java
+++ leetcode-solutions/easy/816-design-hashset/design-hashset.java
@@ -1,32 +1,40 @@
 class MyHashSet {
-    private boolean[] data;
+    private static final int SIZE = 1000; // Size of the array
+    private List<Integer>[] data; // Array of lists (buckets)
 
     // Constructor
     public MyHashSet() {
-        data = new boolean[1000001]; // Size of array is 10^6 + 1
+        data = new List[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            data[i] = new ArrayList<>();
+        }
+    }
+    
+    // Hash function to get the index
+    private int hash(int key) {
+        return key % SIZE;
     }
 
     // Add key to the hash set
     public void add(int key) {
-        data[key] = true;
+        int index = hash(key);
+        List<Integer> bucket = data[index];
+        if (!bucket.contains(key)) {
+            bucket.add(key);
+        }
     }
 
     // Check if the key is in the hash set
     public boolean contains(int key) {
-        return data[key];
+        int index = hash(key);
+        List<Integer> bucket = data[index];
+        return bucket.contains(key);
     }
 
     // Remove the key from the hash set
     public void remove(int key) {
-        data[key] = false;
+        int index = hash(key);
+        List<Integer> bucket = data[index];
+        bucket.remove(Integer.valueOf(key));
     }
 }
-
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */
