59310ad8ce79df753f3f8011360199bfc58e3a3e
Kapil <66022116+immortals-ume@users.noreply.github.com>
2025-01-18 00:46:56 +0530

    Time: 2 ms (100.00%) | Memory: 62.7 MB (26.47%) - LeetSync

--- leetcode-solutions/easy/2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ leetcode-solutions/easy/2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -1,23 +1,9 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-         int n = derived.length;
-        
-       
-        
-        // Check both possibilities for original[0]: 0 and 1
-        return isValid(0,n,derived) || isValid(1,n,derived);
-    }
-     // Helper function to validate the circular XOR condition
-        boolean isValid(int start,int n,int[] derived) {
-            int[] original = new int[n];
-            original[0] = start;
-            
-            // Generate the rest of the original array
-            for (int i = 1; i < n; i++) {
-                original[i] = original[i - 1] ^ derived[i - 1];
-            }
-            
-            // Check the circular condition
-            return (original[n - 1] ^ original[0]) == derived[n - 1];
+ int res = 0;
+        for (int x : derived) {
+            res ^= x;
         }
+        return res == 0;
+    }
 }
