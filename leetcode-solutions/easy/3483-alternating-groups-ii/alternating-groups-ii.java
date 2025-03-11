3ff93eeabcac1f9ad8e68ba65923649cbb0e8a2d
Kapil <66022116+immortals-ume@users.noreply.github.com>
2025-03-11 15:31:49 +0530

    Time: 2 ms (99.04%) | Memory: 63.3 MB (33.21%) - LeetSync

--- leetcode-solutions/easy/3483-alternating-groups-ii/alternating-groups-ii.java
+++ leetcode-solutions/easy/3483-alternating-groups-ii/alternating-groups-ii.java
@@ -1,50 +1,18 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
- int n = colors.length;
-        int count = 0;
-        int validWindows = 0;
-        
-        // Initial check for the first window of size k
-        boolean isValid = true;
-        for (int i = 0; i < k - 1; i++) {
-            if (colors[i] == colors[i + 1]) {
-                isValid = false;
-                break;
-            }
-        }
-        if (isValid) validWindows++;
-
-        // Sliding window check for the rest
-        for (int i = 1; i < n; i++) {
-            // Remove the previous first element, add the new one
-            int prevIndex = (i - 1) % n;
-            int newIndex = (i + k - 1) % n;
-            
-            // Check the transition at the front
-            if (colors[prevIndex] == colors[(prevIndex + 1) % n]) {
-                isValid = false;
-            }
-            
-            // Check the transition at the new back
-            if (colors[(newIndex - 1 + n) % n] == colors[newIndex]) {
-                isValid = false;
-            }
-            
-            // If the window was valid before and this change didn't break it
-            if (isValid) validWindows++;
+        int cnt = 1;
+        int res = 0;
+        int firstcnt = 0;
+        for (int i = 1; i < colors.length; i++) {
+            if (colors[i] != colors[i-1]) cnt++;
             else {
-                // Recalculate the whole window if needed
-                isValid = true;
-                for (int j = i; j < i + k - 1; j++) {
-                    if (colors[j % n] == colors[(j + 1) % n]) {
-                        isValid = false;
-                        break;
-                    }
-                }
-                if (isValid) validWindows++;
+                if (firstcnt == 0) firstcnt = Math.min(k-1, cnt); 
+                cnt = 1;
             }
+            if (cnt >= k) res++;
         }
-
-        return validWindows;
+        if (colors[colors.length-1] == colors[0]) return res;
+        if (cnt == colors.length) return cnt;
+        return res + Math.max(0, firstcnt + Math.min(cnt, k-1) - k + 1);
     }
 }
