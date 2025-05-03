aee87d88043e8bacb6ce686da6109ae480967d5c
Kapil <66022116+immortals-ume@users.noreply.github.com>
2025-05-03 11:00:31 +0530

    Time: 25 ms (41.51%) | Memory: 45.9 MB (47.17%) - LeetSync

---
+++ leetcode-solutions/easy/868-push-dominoes/push-dominoes.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String pushDominoes(String dominoes) {
+        int n = dominoes.length();
+        int[] forces = new int[n];
+        int force = 0;
+
+        // Left to right
+        for (int i = 0; i < n; i++) {
+            char c = dominoes.charAt(i);
+            if (c == 'R') {
+                force = n;
+            } else if (c == 'L') {
+                force = 0;
+            } else {
+                force = Math.max(force - 1, 0);
+            }
+            forces[i] += force;
+        }
+
+        // Right to left
+        force = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            char c = dominoes.charAt(i);
+            if (c == 'L') {
+                force = n;
+            } else if (c == 'R') {
+                force = 0;
+            } else {
+                force = Math.max(force - 1, 0);
+            }
+            forces[i] -= force;
+        }
+
+        // Build the result
+        StringBuilder result = new StringBuilder();
+        for (int f : forces) {
+            if (f == 0) {
+                result.append('.');
+            } else if (f > 0) {
+                result.append('R');
+            } else {
+                result.append('L');
+            }
+        }
+
+        return result.toString();
+    }
+}
