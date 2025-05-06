1b99eb40f328745d608ade83df2c3e7c5f09c739
Kapil <66022116+immortals-ume@users.noreply.github.com>
2025-05-07 00:43:25 +0530

    Time: 117 ms (99.78%) | Memory: 124.8 MB (5.14%) - LeetSync

--- leetcode-solutions/easy/1023-time-based-key-value-store/time-based-key-value-store.java
+++ leetcode-solutions/easy/1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,46 +1,37 @@
 class TimeMap {
 
- // Key -> List of (timestamp, value) pairs
-    private Map<String, List<Pair>> map;
+ private  Node head;
 
     public TimeMap() {
-        map = new HashMap<>();
+        head = new Node();
     }
 
     public void set(String key, String value, int timestamp) {
-        map.computeIfAbsent(key, k -> new ArrayList<>()).add(new Pair(timestamp, value));
+        head = new Node(key, value, timestamp, head);
     }
 
     public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) return "";
-
-        List<Pair> list = map.get(key);
-        int left = 0;
-        int right = list.size() - 1;
-        String result = "";
-
-        // Binary search for the largest timestamp <= given timestamp
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (list.get(mid).timestamp <= timestamp) {
-                result = list.get(mid).value;
-                left = mid + 1; // try to find a later one
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        return result;
+        return dfs(key, timestamp, head);
     }
 
-    // Custom class for timestamp-value pair
-    private static class Pair {
-        int timestamp;
-        String value;
+    private String dfs(String key, int timestamp, Node node) {
+        if(node == null) return new String();
+        if(key.equals(node.key) && timestamp >= node.timestamp) return node.value;
 
-        Pair(int timestamp, String value) {
-            this.timestamp = timestamp;
-            this.value = value;
+        return dfs(key, timestamp, node.prev);
+    }
+
+    class Node {
+        String key, value;
+        int timestamp;
+        Node prev;
+
+        Node(){}
+        Node(String _key, String _value, int _timestamp, Node _prev) {
+            key = _key;
+            value = _value;
+            timestamp = _timestamp;
+            prev = _prev;
         }
     }
 }
