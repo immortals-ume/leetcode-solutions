1210355570054bb96913f181447a4c15b379169c
Kapil <66022116+immortals-ume@users.noreply.github.com>
2025-05-07 01:17:14 +0530

    Time: 35 ms (71.86%) | Memory: 46.9 MB (22.53%) - LeetSync

--- leetcode-solutions/easy/1905-design-authentication-manager/design-authentication-manager.java
+++ leetcode-solutions/easy/1905-design-authentication-manager/design-authentication-manager.java
@@ -1,31 +1,49 @@
 class AuthenticationManager {
 
-       private final int timeToLive;
+      private final int timeToLive;
     private final Map<String, Integer> tokenExpiry;
+    private final PriorityQueue<Pair> minHeap;
+
+    private static class Pair {
+        String tokenId;
+        int expiryTime;
+
+        Pair(String tokenId, int expiryTime) {
+            this.tokenId = tokenId;
+            this.expiryTime = expiryTime;
+        }
+    }
 
     public AuthenticationManager(int timeToLive) {
         this.timeToLive = timeToLive;
         this.tokenExpiry = new HashMap<>();
+        this.minHeap = new PriorityQueue<>(Comparator.comparingInt(p -> p.expiryTime));
     }
 
     public void generate(String tokenId, int currentTime) {
-        tokenExpiry.put(tokenId, currentTime + timeToLive);
+        int expiry = currentTime + timeToLive;
+        tokenExpiry.put(tokenId, expiry);
+        minHeap.offer(new Pair(tokenId, expiry));
     }
 
     public void renew(String tokenId, int currentTime) {
         if (tokenExpiry.containsKey(tokenId) && tokenExpiry.get(tokenId) > currentTime) {
-            tokenExpiry.put(tokenId, currentTime + timeToLive);
+            int newExpiry = currentTime + timeToLive;
+            tokenExpiry.put(tokenId, newExpiry);
+            minHeap.offer(new Pair(tokenId, newExpiry));
         }
     }
 
     public int countUnexpiredTokens(int currentTime) {
-        int count = 0;
-        for (int expiryTime : tokenExpiry.values()) {
-            if (expiryTime > currentTime) {
-                count++;
+        // Cleanup expired tokens from the heap
+        while (!minHeap.isEmpty() && minHeap.peek().expiryTime <= currentTime) {
+            Pair expired = minHeap.poll();
+            // Only remove from map if this is the latest expiry
+            if (tokenExpiry.getOrDefault(expired.tokenId, 0) == expired.expiryTime) {
+                tokenExpiry.remove(expired.tokenId);
             }
         }
-        return count;
+        return tokenExpiry.size();
     }
 }
 
