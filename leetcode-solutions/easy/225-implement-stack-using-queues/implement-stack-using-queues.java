b0d2b44e513b23386d5066a0a83f57644857685f
Kapil <66022116+immortals-ume@users.noreply.github.com>
2025-05-06 18:54:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (64.68%) - LeetSync

--- leetcode-solutions/easy/225-implement-stack-using-queues/implement-stack-using-queues.java
+++ leetcode-solutions/easy/225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,37 +1,65 @@
-import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 class MyStack {
-    private Queue<Integer> queue ;
-    
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
     public MyStack() {
-        queue = new LinkedList<>();
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
     }
-    
+
+    // Push element x onto stack
     public void push(int x) {
-        queue.add(x);
-        int size = queue.size();
-        
-        // Rotate the queue to bring the new element to the front
-        for (int i = 0; i < size - 1; i++) {
-            queue.add(queue.remove());
-        }
+        queue1.offer(x);  // O(1)
     }
-    
+
+    // Removes the element on the top of the stack and returns that element
     public int pop() {
-      return queue.remove();
+        // Move all but the last element from queue1 to queue2
+        while (queue1.size() > 1) {
+            queue2.offer(queue1.poll());
+        }
+        // The last element in queue1 is the top of the stack
+        int top = queue1.poll();
+        
+        // Swap the references of the queues
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+        
+        return top;  // O(n) for the movement of elements, O(1) for the poll
     }
-    
+
+    // Get the top element
     public int top() {
-        return queue.peek();
+        // Move all but the last element from queue1 to queue2
+        while (queue1.size() > 1) {
+            queue2.offer(queue1.poll());
+        }
+        int top = 0;
+
+        // The last element in queue1 is the top of the stack
+        if(!queue1.isEmpty())
+            top = queue1.peek();
+        
+        // Move it to queue2 to maintain the stack order
+        queue2.offer(queue1.poll());
+        
+        // Swap the references of the queues
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+        
+        return top;  // O(n) for the movement of elements, O(1) for the peek
     }
-    
+
+    // Returns whether the stack is empty
     public boolean empty() {
-        return queue.isEmpty();
+        return queue1.isEmpty();
     }
 }
-
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack obj = new MyStack();
