3fcb4e8eb33c04673fe6439549be095027271de8
Kapil <66022116+immortals-ume@users.noreply.github.com>
2025-05-07 17:56:48 +0530

    Time: 14 ms (53.75%) | Memory: 49.9 MB (5.98%) - LeetSync

---
+++ leetcode-solutions/easy/744-network-delay-time/network-delay-time.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] time : times) {
+            graph.computeIfAbsent(time[0], x -> new ArrayList<>()).add(new int[]{time[1], time[2]});
+        }
+
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.offer(new int[]{k, 0});
+
+        while (!pq.isEmpty()) {
+            int[] current = pq.poll();
+            int node = current[0], time = current[1];
+
+            if (!graph.containsKey(node)) continue;
+
+            for (int[] neighbor : graph.get(node)) {
+                int nextNode = neighbor[0], weight = neighbor[1];
+                if (dist[nextNode] > time + weight) {
+                    dist[nextNode] = time + weight;
+                    pq.offer(new int[]{nextNode, dist[nextNode]});
+                }
+            }
+        }
+
+        int maxTime = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) return -1;
+            maxTime = Math.max(maxTime, dist[i]);
+        }
+        return maxTime;
+    }
+}
