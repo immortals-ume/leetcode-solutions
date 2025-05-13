17f573239bb9fabfaf5d00cf6a83b21f487ec16a
Kapil <66022116+immortals-ume@users.noreply.github.com>
2025-05-13 10:46:55 +0530

    Time: 203 ms (40.51%) | Memory: 72.2 MB (11.39%) - LeetSync

--- leetcode-solutions/easy/3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.java
+++ leetcode-solutions/easy/3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.java
@@ -1,32 +1,30 @@
 class Solution {
      private static final int MOD = 1_000_000_007;
     public int lengthAfterTransformations(String s, int t) {
-         int[] counts = new int[26];  // counts[i] = number of 'a' + i characters
+        int[][] dp = new int[t + 1][26];
 
-        // Initialize counts from input string
+        // Initial character frequencies from string
         for (char ch : s.toCharArray()) {
-            counts[ch - 'a']++;
+            dp[0][ch - 'a']++;
         }
 
-        for (int step = 0; step < t; step++) {
-            int[] newCounts = new int[26];
-
-            // a -> y shift to next char
-            for (int i = 0; i < 25; i++) {
-                newCounts[i + 1] = (newCounts[i + 1] + counts[i]) % MOD;
+        // Fill DP table
+        for (int i = 1; i <= t; i++) {
+            for (int c = 0; c < 26; c++) {
+                int count = dp[i - 1][c];
+                if (c < 25) {
+                    dp[i][c + 1] = (dp[i][c + 1] + count) % MOD;
+                } else {
+                    dp[i][0] = (dp[i][0] + count) % MOD;  // 'z' -> 'a'
+                    dp[i][1] = (dp[i][1] + count) % MOD;  // 'z' -> 'b'
+                }
             }
-
-            // z -> ab
-            newCounts[0] = (newCounts[0] + counts[25]) % MOD;
-            newCounts[1] = (newCounts[1] + counts[25]) % MOD;
-
-            counts = newCounts;
         }
 
-        // Total characters = sum of counts
+        // Sum all characters at time t
         long total = 0;
-        for (int count : counts) {
-            total = (total + count) % MOD;
+        for (int c = 0; c < 26; c++) {
+            total = (total + dp[t][c]) % MOD;
         }
 
         return (int) total;
